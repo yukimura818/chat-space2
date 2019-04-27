34b812ab3e30caacb355bdee5ccea2d9e8915952
yu-kimura <yukumura870@gmail.com>
2019-04-27 14:31:42 +0900

    メッセージ送信の非同期化

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,67 @@
+$(function () {
+    function buildHTML(message) {
+        if (message.image) {
+            var html =
+                `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.date}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+        <asset_path src=${message.image} >
+      </div>`
+            return html;
+        } else {
+            var html =
+                `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.date}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+            return html;
+        };
+    }
+    $('.js-form').on('submit', function (e) {
+        e.preventDefault();
+        var formData = new FormData(this);
+        var url = window.location.href
+        console.log(url);
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: formData,
+            dataType: 'json',
+            processData: false,
+            contentType: false
+        })
+            .done(function (data) {
+                console.log('OK');
+                var html = buildHTML(data);
+                $('.messages').append(html);
+                $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
+                $('.js-form')[0].reset();
+            })
+            .fail(function () {
+                alert('error');
+            });
+        return false;
+    });
+});
--- config/application.rb
+++ config/application.rb
@@ -6,13 +6,17 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ChatSpace2
+module ChatSpace
   class Application < Rails::Application
-  config.generators do |g|
+     config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
+      config.i18n.default_locale = :ja
     end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
