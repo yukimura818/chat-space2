4116bf8b19a3ca075dc45d581e563bc5fb094595
yu-kimura <yukumura870@gmail.com>
2019-04-27 15:39:58 +0900

    メッセージ機能の非同期化

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(function () {
-    function buildHTML(message) {
-        if (message.image) {
-            var html =
-                `<div class="message" data-message-id=${message.id}>
+  function buildmessageHTML(message) {
+    if (message.image) {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
         <div class="upper-message">
           <div class="upper-message__user-name">
             ${message.user_name}
@@ -18,10 +18,10 @@ $(function () {
         </div>
         <asset_path src=${message.image} >
       </div>`
-            return html;
-        } else {
-            var html =
-                `<div class="message" data-message-id=${message.id}>
+      return html;
+    } else {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
         <div class="upper-message">
           <div class="upper-message__user-name">
             ${message.user_name}
@@ -36,32 +36,32 @@ $(function () {
           </p>
         </div>
       </div>`
-            return html;
-        };
-    }
-    $('.js-form').on('submit', function (e) {
-        e.preventDefault();
-        var formData = new FormData(this);
-        var url = window.location.href
-        console.log(url);
-        $.ajax({
-            url: url,
-            type: "POST",
-            data: formData,
-            dataType: 'json',
-            processData: false,
-            contentType: false
-        })
-            .done(function (data) {
-                console.log('OK');
-                var html = buildHTML(data);
-                $('.messages').append(html);
-                $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
-                $('.js-form')[0].reset();
-            })
-            .fail(function () {
-                alert('error');
-            });
-        return false;
-    });
+      return html;
+    };
+  }
+  $('.js-form').on('submit', function (e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = window.location.href
+    console.log(url);
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+      .done(function (message) {
+        console.log('OK');
+        var html = buildmessageHTML(message);
+        $('.messages').append(html);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
+        $('.js-form')[0].reset();
+      })
+      .fail(function () {
+        alert('error');
+      });
+    return false;
+  });
 });
