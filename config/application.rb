5cdd7f1a947d90bd814f42f9547aae90a923f69a
yu-kimura <yukumura870@gmail.com>
2019-05-02 19:04:11 +0900

    add

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module ChatSpace
       g.helper false
       g.test_framework false
       config.i18n.default_locale = :ja
+      config.time_zone = 'Tokyo'
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
